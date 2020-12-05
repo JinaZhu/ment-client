9ec3f195d1b21a007218cfa57edca082b3c25018
Jina Zhu <jinazhu87@gmail.com>
2020-12-05 15:10:43 -0800

    remove styles from sign-up

--- src/components/SignUp/styled.js
+++ src/components/SignUp/styled.js
@@ -1,18 +1,6 @@
 import styled, { css, keyframes } from "styled-components";
 import { Link } from "react-router-dom";
 
-export const ChoiceContainer = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-export const H1 = styled.h1`
-  font-size: 50px;
-`;
-
 export const SelectionButton = styled.button`
   background-color: transparent;
   color: #474f58;
@@ -40,19 +28,6 @@ export const SelectionButton = styled.button`
   }
 `;
 
-const blink = keyframes`
-0% {
-  opacity: 0;
-}
-100% {
-  opacity: 1;
-}
-`;
-
-export const Underscore = styled.span`
-  animation: ${blink} 0.5s infinite ease;
-`;
-
 export const RedirectLink = styled(Link)`
   text-decoration: none;
   color: #474f58;
@@ -66,19 +41,6 @@ export const Form = styled.form`
   justify-content: center;
   width: 80%;
   margin: 20px 10%;
-`;
-
-export const Input = styled.input`
-  color: #474f58;
-  border: none;
-  border-bottom: 1px solid #474f58;
-  background-color: transparent;
-  font-family: "Darker Grotesque", sans-serif;
-  font-size: 30px;
-  outline: none;
-  width: ${(props) => props.year || "300px"};
-  margin-bottom: 25px;
-  text-align: center;
 `;
 
 export const BackButtonContainer = styled.div`
@@ -150,20 +112,4 @@ export const Textarea = styled.textarea`
   width: 350px;
   height: 100px;
   margin-bottom: 25px;
-`;
-
-export const SignUpButton = styled.button`
-  color: #474f58;
-  border: 1px solid #474f58;
-  background-color: transparent;
-  font-family: "Darker Grotesque", sans-serif;
-  font-size: 30px;
-  outline: none;
-  padding: 0 15px;
-  cursor: pointer;
-
-  :hover {
-    color: #ffeebc;
-    background-color: #474f58;
-  }
 `;
