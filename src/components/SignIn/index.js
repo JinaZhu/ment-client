c33cd5e216aebcb5e15ec2d0531730a4867705d6
Jina Zhu <jinazhu87@gmail.com>
2020-12-05 14:25:28 -0800

    create sign in page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { SignInContainer, Form, H1, Input, SignInButton } from "./styled";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>
-      <p>Sign In</p>
-    </div>
+    <SignInContainer>
+      <H1>Sign In</H1>
+      <Form>
+        <Input
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <Input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <SignInButton>Sign In</SignInButton>
+      </Form>
+    </SignInContainer>
   );
 };
 
